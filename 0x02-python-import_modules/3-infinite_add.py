907a54080ba3e7b0a5e237dd04f2de0370ad34ac
Ahmerd196 <Ahmerd196@gmail.com>
2022-12-01 10:18:59 -0800

    importing modules in python

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python3
-import sys from argv
 if __name__ == "__main__":
-    result = 0
-    if len(argv) == 1:
-        print("0")
-    else:
-        for i in range(1, len(argv)):
-            result += int(argv[i])
-            print(result)
+    from sys import argv
+    res = 0
+    for arg in argv[1:]:
+        res += int(arg)
+        print("{:d}".format(res))
