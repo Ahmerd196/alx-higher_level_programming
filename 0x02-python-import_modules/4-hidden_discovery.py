36b2eee0ff9684628d2457308ed18066f8246077
Ahmerd196 <Ahmerd196@gmail.com>
2022-12-01 18:20:27 -0800

    importing modules in python

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
 import hidden_4
+
 if __name__ == "__main__":
-    for i in dir(hidden_4):
-        if i[0:2] != "__":
-            print("{:s}".format(i))
+
+        for i in dir(hidden_4):
+
+            if i[0:2] != "__":
+
+                print("{:s}".format(i))
