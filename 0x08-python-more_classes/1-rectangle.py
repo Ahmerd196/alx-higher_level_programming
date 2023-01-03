891dd324a068cf76149eab4937cc35d4062e7732
Ahmerd196 <Ahmerd196@gmail.com>
2023-01-02 19:18:39 -0800

    python classes

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -6,7 +6,8 @@ class Rectangle:
     """this represents a rectangle"""
 
     def __init__(self, width=0, height=0):
-    """Initializing this rectangle class
+    """
+    Initializing this rectangle class
     Args:
     width: represents the width of the rectangle
     height: represents the height of the rectangle
