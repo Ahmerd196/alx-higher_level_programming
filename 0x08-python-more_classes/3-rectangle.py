dc377e4a1dd36d65ad6dd7780000add02668891f
Ahmerd196 <Ahmerd196@gmail.com>
2023-01-03 08:19:39 -0800

    python classes

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -4,6 +4,7 @@
 
 class Rectangle:
     """this represents a rectangle"""
+
     def __init__(self, width=0, height=0):
     """Initializing this rectangle class
     Args:
@@ -43,14 +44,17 @@ class Rectangle:
         if value < 0:
             raise ValueError("height must be >= 0")
         self.__height = value
+
     def area(self):
         """Returns the area of the rectangle"""
         return (self.__width * self.__height)
+
     def perimeter(self):
         """Returns the perimeter of the rectangle"""
         if self.__width == 0 or self.__height == 0:
             return (0)
         return ((self.__width * 2) + (self.__height * 2))
+
     def __str__(self) -> str:
         """presents a diagram of the rectangle defined for an object"""
         if self.__width == 0 or self.__height == 0:
