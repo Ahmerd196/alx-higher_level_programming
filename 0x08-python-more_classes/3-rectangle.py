e2814eb63c1d16f1c2e446d2259407c64c6bacc5
Ahmerd196 <Ahmerd196@gmail.com>
2023-01-03 03:42:09 -0800

    python classes

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -18,11 +18,13 @@ class Rectangle:
     self.height = height
    
    @property
+   
    def width(self):
        """retrieves width attribute"""
        return self.__width
 
    @width.setter
+   
    def width(self, value):
        """sets width attribute"""
        if not isinstance(value, int):
@@ -32,11 +34,13 @@ class Rectangle:
        self.__width = value
 
        @property
+       
        def height(self):
            """retrieves height attribute"""
            return self.__height
 
        @height.setter
+       
        def height(self, value):
            """sets height attribute"""
            if not isinstance(value, int):
@@ -44,14 +48,17 @@ class Rectangle:
            if value < 0:
                raise ValueError("height must be >= 0")
            self.__height = value
+           
            def area(self):
                """Returns the area of the rectangle"""
                return (self.__width * self.__height)
+           
            def perimeter(self):
                """Returns the perimeter of the rectangle"""
                if self.__width == 0 or self.__height == 0:
                    return (0)
                return ((self.__width * 2) + (self.__height * 2))
+
            def __str__(self) -> str:
                """presents a diagram of the rectangle defined for an object"""
                if self.__width == 0 or self.__height == 0:
