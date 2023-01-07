7b97a6dd20f39f00479776660a5a572ff1416dcd
Ahmerd196 <Ahmerd196@gmail.com>
2023-01-06 16:56:51 -0800

    tests driven development

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -1,33 +1,23 @@
 #!/usr/bin/python3
 """
-This module defines function to divide two list int a matrix
+This module is composed by a function that adds two numbers
 """
 
 
-def matrix_divided(matrix, div):
+def add_integer(a, b=98):
+    """ Function that adds two integer and/or float numbers
+    Args:
+        a: first number
+        b: second number
+    Returns:
+        The addition of the two given numbers
+    Raises:
+        TypeError: If a or b aren't integer and/or float numbers
     """
-    Divides list and Raises TypeError
-    """
-    if not isinstance(div, (int, float)):
-        raise TypeError("div must be a number")
-    elif div is 0:
-        raise ZeroDivisionError("division by zero")
-    typeErr = "matrix must be a matrix (list of lists) of integers/floats"
-    sizeErr = "Each row of the matrix must have the same size"
-    new = []
-    if matrix is None or len(matrix) is 0 or len(matrix[0]) is 0:
-        raise TypeError(typeErr)
-    old = len(matrix[0])
-    for count, y in enumerate(matrix):
-        if not isinstance(y, list):
-            raise TypeError(typeErr)
-        if len(y) != old:
-            raise TypeError(sizeErr)
-        old = len(y)
-        new.append(y[:])
-        for a, item in enumerate(y):
-            if not isinstance(item, (int, float)):
-                raise TypeError(typeErr)
-            new[count][a] = round(item / div, 2)
-    else:
-        return (new)
+    if not isinstance(a, int) and not isinstance(a, float):
+        raise TypeError("a must be an integer")
+    if not isinstance(b, int) and not isinstance(b, float):
+        raise TypeError("b must be an integer")
+    a = int(a)
+    b = int(b)
+    return (a + b)
