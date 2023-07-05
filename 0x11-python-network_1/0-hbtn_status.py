60a65570c6e86729e7751aee6fb05cc668d6b1e4
Ahmerd196 <ahmerd196@gmail.com>
2023-07-05 13:54:35 -0700

    my commit

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,11 @@
+ #!/usr/bin/python3
+import urllib.request
+
+url = 'https://alx-intranet.hbtn.io/status'
+
+with urllib.request.urlopen(url) as response:
+    body = response.read().decode('utf-8')
+
+print('- Body response:')
+print('\t- type:', type(body))
+print('\t- content:', body)
---
+++ 0x11-python-network_1/README.md
@@ -0,0 +1,1 @@
+readme for network 1
