4ff631aa7872ebdcf6393766c225a66711015e3f
Ahmerd196 <ahmerd196@gmail.com>
2023-07-05 14:18:08 -0700

    my commit

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -11,7 +11,7 @@ if __name__ == "__main__":
     with urllib.request.urlopen(url) as response:
         body = response.read()
 
-    print('- Body response:')
+    print('Body response:')
     print('\t- type:', type(body))
     print('\t- content:', body)
     print('\t- utf8 content:', body.decode('utf-8'))
