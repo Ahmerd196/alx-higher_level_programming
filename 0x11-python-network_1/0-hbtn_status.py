36f1ec6ddce5011ecf25aaf63c824f45f3161ac8
Ahmerd196 <ahmerd196@gmail.com>
2023-07-05 14:06:53 -0700

    my commit

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,12 +1,17 @@
 #!/usr/bin/python3
-""" Python script that fetches https://intranet.hbtn.io/status """
+"""
+Module to fetch the status from https://alx-intranet.hbtn.io/status
+"""
+
 import urllib.request
 
-url = 'https://alx-intranet.hbtn.io/status'
+if __name__ == "__main__":
+    url = 'https://alx-intranet.hbtn.io/status'
 
-with urllib.request.urlopen(url) as response:
-    body = response.read().decode('utf-8')
+    with urllib.request.urlopen(url) as response:
+        body = response.read()
 
-print('- Body response:')
-print('\t- type:', type(body))
-print('\t- content:', body)
+    print('- Body response:')
+    print('\t- type:', type(body))
+    print('\t- content:', body)
+    print('\t- utf8 content:', body.decode('utf-8'))
