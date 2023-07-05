3fa992b06896be1f7e86716c2cda1788d58b6cf8
Ahmerd196 <ahmerd196@gmail.com>
2023-07-05 14:55:12 -0700

    my commit

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,15 +1,11 @@
 #!/usr/bin/python3
 """
-Module to fetch a URL and display the body of the response.
+Python script that fetches an URL with requests package
 """
-
 import requests
 
-if __name__ == "__main__":
-    url = "https://alx-intranet.hbtn.io/status"
-    response = requests.get(url)
-    body = response.text
 
-    print("Body response:")
-    print("\t- type: {}".format(type(body)))
-    print("\t- content: {}".format(body))
+if __name__ == "__main__":
+    r = requests.get('https://intranet.hbtn.io/status')
+    t = r.text
+    print('Body response:\n\t- type: {}\n\t- content: {}'.format(type(t), t))
