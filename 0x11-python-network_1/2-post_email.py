1de6e2601c035f4e3ccb0de1c93ba531bbd12ac6
Ahmerd196 <ahmerd196@gmail.com>
2023-07-05 14:42:44 -0700

    my commit

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,20 +1,18 @@
 #!/usr/bin/python3
 """
-Module to send a POST request to a URL with an email parameter and display the response body.
+Python script that takes in a URL and an email, sends a POST
+request to the passed URL with the email as a parameter, and
+displays the body of the response
 """
-
-import urllib.parse
-import urllib.request
+from urllib import request, parse
 import sys
 
+
 if __name__ == "__main__":
-    url = sys.argv[1]
-    email = sys.argv[2]
-
-    data = urllib.parse.urlencode({'email': email}).encode('utf-8')
-    req = urllib.request.Request(url, data=data, method='POST')
-
-    with urllib.request.urlopen(req) as response:
-        body = response.read().decode('utf-8')
-
-    print(body)
+    values = {'email': sys.argv[2]}
+    data = parse.urlencode(values)
+    data = data.encode('ascii')
+    req = request.Request(sys.argv[1], data)
+    with request.urlopen(req) as response:
+        body = response.read()
+        print(body.decode('utf-8'))
